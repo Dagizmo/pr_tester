86389f6df2698cfc1cdbe5baa85929b6d3e255c1
Jose De Jesus Medina Rios <jomedi@microsoft.com>
2019-07-22 14:35:28 -0700

    add comments

--- ZZZ/random.cs
+++ ZZZ/random.cs
@@ -3,6 +3,9 @@ using System.Collections;
 int main()
 {
     var a = 0;
+    //Adding additional information
+    //To check if position of comments will be changed after this.
+    // TEEEEEST _______
     var name = "Hello";
     var b = 0;
     var other = 3;
